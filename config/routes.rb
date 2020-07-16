6ee34073f70f591e6fdf956de47abcc644fa23c1
evertsalinas <evertsalinas88@gmail.com>
2020-07-16 17:30:27 -0500

    Add admin user and basic gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin_users
+
+  # devise_scope :admin_users do
+  #   get 'sign_in',  to: 'devise/sessions#new'
+  #   get 'sign_out', to: 'devise/sessions#destroy'
+  # end
 end
