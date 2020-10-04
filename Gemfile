f136db51e1e8ea8b57146a2c9c283d1a145a932a
evertsalinas <evertsalinas88@gmail.com>
2020-10-04 17:22:29 -0500

    Remove administrate remains

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 
 # Admin framework
-gem "administrate"
+gem 'activeadmin'
 
 # Authorization
 gem "pundit"
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -25,7 +25,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable #, :registerable, :recoverable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   ROLES = %w(master contaduria).freeze
 
