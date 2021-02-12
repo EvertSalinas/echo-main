0c6b04f897a4a5b6e22cd76312b988639721a753
evertsalinas <evertsalinas88@gmail.com>
2021-02-11 21:52:06 -0600

    fix scope

--- app/admin/payment_logs.rb
+++ app/admin/payment_logs.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register PaymentLog do
   menu priority: 2
   permit_params :folio, :client_id, :invoice_id, :seller_id, :total_amount, :voucher
 
-  searchable_select_options(scope: PaymentLog.abierto,
+  searchable_select_options(scope: Proc.new { PaymentLog.abierto } ,
                             text_attribute: :voucher)
 
   index do
